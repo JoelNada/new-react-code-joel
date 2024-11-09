1a71a43c285abaad7aa07c7e8282f793e0891575
JoelNada <joelnada789@gmail.com>
2024-11-09 10:14:36 +0530

    js code

--- practice.js
+++ practice.js
@@ -10,3 +10,20 @@ console.error("obj");
 console.table(obj);
 
 console.log(Array.isArray(names));
+
+const obj2 = {};
+
+names.forEach((name) =>
+  name.startsWith("j") ? (obj2[name] = (obj2[name] || 0) + 1) : ""
+);
+
+let sentence = "My name is joel";
+
+console.log(
+  sentence
+    .split(" ")
+    .map((word) => word.split("").reverse().join(""))
+    .join(" ")
+);
+
+console.log(obj2);
